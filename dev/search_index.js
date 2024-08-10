0460cd2ca0fbdd9989b10c413f68f7837f5c8618
Documenter.jl <documenter@juliadocs.github.io>
2024-08-10 11:28:08 +0000

    build based on f687400

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Sgmam","category":"page"},{"location":"#Sgmam","page":"Home","title":"Sgmam","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Sgmam.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Sgmam]","category":"page"}]
+}
