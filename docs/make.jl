6eb2df759b5c05e91ca036e343165bad0f73cc3f
oameye <orjan.ameye@hotmail.com>
2024-08-12 07:43:18 +0200

    try documentervitepress

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,8 @@ CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== 
 
 using Sgmam
 using Documenter
+using DocumenterVitepress
+using DocumenterCitations
 
 DocMeta.setdocmeta!(Sgmam, :DocTestSetup, :(using Sgmam); recursive=true)
 
@@ -11,18 +13,30 @@ Downloads.download(
   joinpath(@__DIR__, "src/index.md"),
 )
 
+bib = CitationBibliography(
+    joinpath(@__DIR__, "src", "refs.bib");
+    style=:numeric  # default
+)
+
 makedocs(;
   modules=[Sgmam],
+  repo=Remotes.GitHub("oameye", "Sgmam.jl"),
   authors="Orjan Ameye <orjan.ameye@hotmail.com>",
   sitename="Sgmam.jl",
-  format=Documenter.HTML(;
-    canonical = "https://oameye.github.io/Sgmam.jl",
-    edit_link = "master",
-    assets    = String[],
+  format=DocumenterVitepress.MarkdownVitepress(;
+    repo="github.com/oameye/Sgmam.jl",
+    devbranch = "master",
+    devurl = "dev",
   ),
   pages=["Home" => "index.md", "API" => "API.md"],
+  plugins = [bib,],
 )
 
 if CI
-  deploydocs(; repo="github.com/oameye/Sgmam.jl", devbranch="master")
+  deploydocs(;
+   repo="github.com/oameye/Sgmam.jl",
+   devbranch="master",
+   target = "build",
+   branch = "gh-pages",
+   push_preview = true,)
 end
