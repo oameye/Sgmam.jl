53fa7ea343ab199a9b0fb672907c7ff9e0274405
Orjan Ameye <orjan.ameye@hotmail.com>
2024-11-21 15:16:09 +0100

    clean up code quality tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,21 +2,24 @@ using Sgmam
 using Test
 
 @testset "Code quality" begin
-    using ExplicitImports, Aqua
-    ignore_deps = [:Random, :Printf, :Test, :Pkg]
+  using ExplicitImports, Aqua
+  ignore_deps = [:Test]
 
-    @test check_no_stale_explicit_imports(Sgmam) == nothing
-    @test check_all_explicit_imports_via_owners(Sgmam) == nothing
-    Aqua.test_ambiguities(Sgmam)
-    Aqua.test_all(
-        Sgmam;
-        deps_compat = (ignore=ignore_deps, check_extras=(ignore=ignore_deps,), check_weakdeps=(ignore=ignore_deps,)),
-        piracies    = (treat_as_own=[],),
-        ambiguities = false,
-    )
+  @test check_no_stale_explicit_imports(Sgmam) == nothing
+  @test check_all_explicit_imports_via_owners(Sgmam) == nothing
+  Aqua.test_ambiguities(Sgmam)
+  Aqua.test_all(
+    Sgmam;
+    deps_compat = (
+        ignore=ignore_deps,
+        check_extras=(ignore=ignore_deps,),
+        check_weakdeps=(ignore=ignore_deps,)
+        ),
+    ambiguities = false,
+  )
 end
 
 @testset "Code linting" begin
-    using JET
-    JET.test_package(Sgmam; target_defined_modules=true)
+  using JET
+  JET.test_package(Sgmam; target_defined_modules=true)
 end
